51a37d69422b7d3f860d688630138a00f941403e
nikostra <niko.stratil@gmail.com>
2022-09-29 14:18:32 +0200

    removed non-working vector solution

--- R/knapsack_brute_force.R
+++ R/knapsack_brute_force.R
@@ -8,36 +8,31 @@ knapsack_objects <-
     v=runif(n = n, 0, 10000)
   )
 
+# test with brute_force_knapsack(knapsack_objects[1:12,],3500)
 #recursive solution
 brute_force_knapsack = function(x,W){
-  v = c()
 
-  knapsack_recursive = function(x,W,n,v){
-    v1 = c()
-    v2 = c()
-    
+  knapsack_recursive = function(x,W,n){
+
     #end case of recursion
     if (n == 0 || W == 0){
       return(0)
     }
     #check if item is too big
     if (x$w[n] > W){
-      return(knapsack_recursive(x,W, n -1,v2))
+      return(knapsack_recursive(x,W, n -1))
     } else {
-      v1 = c(v1,n)
+      v1 = c()
       # if item is improving max value, use it, otherwise ignore it
-      a = x$v[n] + knapsack_recursive(x, W - x$w[n], n - 1,v1)
-      b = knapsack_recursive(x,W, n - 1,v2)
+      a = x$v[n] + knapsack_recursive(x, W - x$w[n], n - 1)
+      b = knapsack_recursive(x,W, n - 1)
       if(a > b){
-        v <- v1
         return(a)
       } else {
-        v <- v2
         return(b)
       }
       #return(max(x$v[n] + knapsack_recursive(x, W - x$w[n], n - 1), knapsack_recursive(x,W, n - 1)))
     }
   }
-  print(v)
   return(knapsack_recursive(x,W,nrow(x)))
 }
