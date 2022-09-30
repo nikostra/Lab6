c789fafbd073b81d9e3647a1e49554dd17c981c4
nikostra <niko.stratil@gmail.com>
2022-09-30 13:31:32 +0200

    added tests, documentation and finished brute_force_knapsack

--- R/knapsack_greedy.R
+++ R/knapsack_greedy.R
@@ -1,4 +1,34 @@
+#' This is a function used to solve the knapsack problem with greedy algorithm
+#'
+#' This function solves the knapsack problem using a greedy algorithm. This does not
+#' guarantee a perfect solution, but runs much faster than other options. In any case
+#' the algorithm will return at least 50% of the original value
+#' The inputs are a data frame of items with a value and a weight and a parameter
+#' determining the total weight limit
+#'  
+#' @param x Data frame with all the elements that can be added to the knapsack.
+#' @param W Weight limit of the knapsack.
+#'  
+#' @export
+#' @importFrom utils tail
+#' 
+#' @examples 
+#' n = 2000
+#' knapsack_objects <-
+#' data.frame(
+#'   w=sample(1:4000, size = n, replace = TRUE),
+#'    v=runif(n = n, 0, 10000)
+#'  )
+#' greedy_knapsack(knapsack_objects[1:800,],3500)
+#' greedy_knapsack(knapsack_objects[1:1200,],2000)
+#' 
+#' 
+#' 
 greedy_knapsack = function(x,W){
+  if(W < 0){
+    stop("The total weight cannot be negatie", call. = FALSE)
+  }
+  
   #setup
   x$value_per_weight = with(x,v/w) # calculating the value / weight ratio
   x$index = seq.int(nrow(x))
