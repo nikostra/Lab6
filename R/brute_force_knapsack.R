a68f07fcd24968dd4e7f4b2b912de3858e7af3d8
unknown <juapa351@student.liu.se>
2022-10-03 00:16:25 +0200

    Added input check for test passing

--- R/brute_force_knapsack.R
+++ R/brute_force_knapsack.R
@@ -20,33 +20,36 @@
 #' brute_force_knapsack(knapsack_objects[1:12,],3500)
 #' brute_force_knapsack(knapsack_objects[1:8,],2000)
 brute_force_knapsack = function(x,W, parallel = FALSE){
-    n <- nrow(x)
-    elements <- c()
-    values <- integer(2^n) # Number of possible combinations
-    for (i in 1:length(values)){
-      # Store combination number as a binary vector to get the active elements for each case
-      elements <- rbind(elements, intToBits(i)[1:n]) 
-      val <- 0
-      weight <- 0
-      for (j in 1:n){
-        if (elements[i,j] == 1){
-          # Add the active elements weights and values
-          weight <- weight + x$w[j]
-          val <- val + x$v[j]
-        }
-      }
-      if (weight <= W){
-        # If weight is below the limit, save the value
-        values[i] <- val
+  if(W < 0){
+    stop("The total weight cannot be negatie", call. = FALSE)
+  }
+  n <- nrow(x)
+  elements <- c()
+  values <- integer(2^n) # Number of possible combinations
+  for (i in 1:length(values)){
+    # Store combination number as a binary vector to get the active elements for each case
+    elements <- rbind(elements, intToBits(i)[1:n]) 
+    val <- 0
+    weight <- 0
+    for (j in 1:n){
+      if (elements[i,j] == 1){
+        # Add the active elements weights and values
+        weight <- weight + x$w[j]
+        val <- val + x$v[j]
       }
     }
-    # Get the maximum value:
-    best_val <- max(values)
-    # Get the active elements from the best case:
-    el <- 1:n
-    index <- which(values == best_val)
-    el <- el[which(elements[index,] == 1)]
-    
-    res <- list("value" = best_val, "elements" = el)
-    return(res)
+    if (weight <= W){
+      # If weight is below the limit, save the value
+      values[i] <- val
+    }
+  }
+  # Get the maximum value:
+  best_val <- max(values)
+  # Get the active elements from the best case:
+  el <- 1:n
+  index <- which(values == best_val)
+  el <- el[which(elements[index,] == 1)]
+  
+  res <- list("value" = best_val, "elements" = el)
+  return(res)
 }
