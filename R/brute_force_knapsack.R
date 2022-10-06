39e195b55cebfec4b5aa2aedb8c95d62bb9e6c24
nikostra <niko.stratil@gmail.com>
2022-10-06 10:21:55 +0200

    fixed parallel brute_force function

--- R/brute_force_knapsack.R
+++ R/brute_force_knapsack.R
@@ -35,41 +35,43 @@ brute_force_knapsack = function(x,W, parallel = FALSE){
       raw_comb <- intToBits(i)
       # Save the binary vector to elements matrix
       elements <- c()
-      for (j in 1:n){
-        if (raw_comb[j] == 1){
-          elements  <- append(elements, 1)
+      elements = sapply(seq_along(1:n),function(j){
+        if(raw_comb[j] == 1){
+          return(1)
+        } else {
+          return(0)
         }
-        else{
-          elements  <- append(elements, 0)
-        }
-      }
-      val <- 0
-      weight <- 0
-      for (j in 1:n){
+      })
+      val_weight = list()
+      val_weight = sapply(1:n, function(j){
         if (elements[j] == 1){
           # Add the active elements weights and values
-          weight <- weight + data$w[j]
-          val <- val + data$v[j]
+          list(val = data$v[j],weight = data$w[j])
+        } else {
+          list(val=0,weight=0)
         }
-      }
-      if (weight <= W){
+      })
+      values = 0
+      if (sum(unlist(val_weight[2,])) <= W){
         # If weight is below the limit, save the value
-        values <- val
+        values <- sum(unlist(val_weight[1,]))
+        return(list(val=values, el=elements))
       }
-      return(values, elements)
     }
     # Get number of cores
     cores <- detectCores() -1
     # Set up the cluster
     c1 <- makeCluster(cores)
-    clusterExport(c1, bfk_fun)
-    clusterExport(c1, x)
-    clusterExport(c1, W)
-    clusterExport(c1, n)
+    clusterExport(c1, varlist = c("bfk_fun","x","W","n"), envir=environment())
     # Run the function in parallel
-    res <- parSapply(c1, 1:2^n, function(i) bfk_fun(i, data = x, W, n))
+    res <- parLapply(c1, 1:2^n, function(i) bfk_fun(i, data = x, W, n))
     # Stop the cluster
     stopCluster(c1)
+    res = res[lengths(res) != 0] # remove NULL entries
+    res = do.call(rbind, res) # transform to usable form from list of lists
+    max_index = which(as.numeric(res[,1]) == max(as.numeric(res[,1]))) # find maximum value
+    els = which((res[[max_index,2]]) == 1) # get elements with max value
+    res = list("value" = res[[max_index,1]], "elements" = els)
     return(res)
   }
   else{ # Original brute force function:
